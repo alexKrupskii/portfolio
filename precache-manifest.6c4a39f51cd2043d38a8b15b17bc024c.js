f5f839d3aaa5b54cb50c19c7186465fb3afd14e9
alexKrupskii <alex.krupskii91@gmail.com>
2020-06-03 00:30:35 +0300

    Updates

---
+++ precache-manifest.6c4a39f51cd2043d38a8b15b17bc024c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c29528f6c2501a19975b1ab83118a70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd0ddcdc6f7be9711b52",
+    "url": "/portfolio/static/css/main.29b3fc0f.chunk.css"
+  },
+  {
+    "revision": "c66c674f1e6e442b32a2",
+    "url": "/portfolio/static/js/2.39d500c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.39d500c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0ddcdc6f7be9711b52",
+    "url": "/portfolio/static/js/main.fc0e5276.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "22077c5f04038bc542ae9a5becaa52ca",
+    "url": "/portfolio/static/media/home_bg.22077c5f.jpg"
+  },
+  {
+    "revision": "30300c277529f0eddf0d6ef4b2fc90fe",
+    "url": "/portfolio/static/media/photo.30300c27.png"
+  }
+]);
